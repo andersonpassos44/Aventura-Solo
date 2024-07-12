dfb4f7cb806bf0b243933e17e7e4a70b0d97cab0
Anderson Passos da Silva <160030687+andersonpassos44@users.noreply.github.com>
2024-07-11 22:09:18 -0300

    update

--- script.js
+++ script.js
@@ -2,9 +2,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Seleciona o vídeo e adiciona um evento de término (ended)
     var video = document.getElementById('vd');
-    video.addEventListener('ended', function() {
-        // Quando o vídeo terminar, mostra o menu
-        var menu = document.getElementById('menu');
-        menu.style.visibility = 'visible';
-    });
+    if (video) {
+        video.addEventListener('ended', function() {
+            // Quando o vídeo terminar, mostra o menu
+            var menu = document.getElementById('menu');
+            if (menu) {
+                menu.style.visibility = 'visible';
+            }
+        });
+    }
+
+    // Seleciona o dropdown e adiciona um evento de clique
+    const dropdown = document.querySelector('.dropdown');
+    if (dropdown) {
+        dropdown.addEventListener('click', function() {
+            this.querySelector('.submenu').classList.toggle('show');
+        });
+    }
 });
